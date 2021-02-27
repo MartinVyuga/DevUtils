d44374fc5cbd7834f6343500507473c609366625
Martin Vyuga <martin.vyuga@scales-group.com>
2021-02-27 10:03:17 +0100

    projects

---
+++ AddTableFindMethodTest/Program.cs
@@ -0,0 +1,117 @@
+﻿using Dynamics.AX.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using DevUtilsD365.AddTableFindMethod;
+using DevUtilsD365.CreateExtensionClass;
+using DevUtilsD365.DataContractBuilder;
+using DevUtilsD365.EnumCreator;
+using DevUtilsD365.Kernel;
+using DevUtilsD365.RunBaseBuilder;
+using DevUtilsD365.TableFieldsBuilder;
+
+namespace AddTableFindMethodTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //TestAddTableFindMethod();
+            //TestCreateExtensionClass();
+            //TestEnumCreator();
+            //TestTableFieldsBuilder();
+            //TestRunBaseBuilder();
+            TestDataContractBuilder();
+        }
+        private static void TestDataContractBuilder()
+        {
+            SnippetsParms snippetsParms = new SnippetsParms();
+            DataContractBuilderParms runBaseBuilder = new DataContractBuilderParms();
+
+            runBaseBuilder.InitDialogParms(snippetsParms);
+
+            SnippetsDialog dialog = new SnippetsDialog();
+            dialog.SetParameters(snippetsParms);
+            Application.Run(dialog);
+        }
+        private static void TestRunBaseBuilder()
+        {
+            SnippetsParms  snippetsParms = new SnippetsParms();
+            RunBaseBuilder runBaseBuilder = new RunBaseBuilder();
+
+            runBaseBuilder.InitDialogParms(snippetsParms);
+
+            SnippetsDialog dialog = new SnippetsDialog();
+            dialog.SetParameters(snippetsParms);
+            Application.Run(dialog);
+        }
+
+        private static void TestTableFieldsBuilder()
+        {
+            TableFieldsBuilderDialog dialog = new TableFieldsBuilderDialog();
+
+            TableFieldsBuilderParms parms = new TableFieldsBuilderParms();
+            parms.TableName = "CustTable";            
+
+            dialog.SetParameters(parms);
+
+            Application.Run(dialog);
+        }
+
+        private static void TestCreateExtensionClass()
+        {
+            CreateExtensionClassDialog dialog = new CreateExtensionClassDialog();
+
+            CreateExtensionClassParms parms = new CreateExtensionClassParms();
+
+            parms.ElementName = "CustTable";
+            parms.ElementType = ExtensionClassObject.Form;
+            parms.Prefix = "MY";
+            //parms.ClassType = ExtensionClassType.EventHandler;
+
+            dialog.SetParameters(parms);
+
+            Application.Run(dialog);
+        }
+
+        private static void TestEnumCreator()
+        {
+            EnumCreatorDialog dialog = new EnumCreatorDialog();
+            EnumCreatorParms parms = new EnumCreatorParms();
+
+            dialog.SetParameters(parms);
+
+            Application.Run(dialog);
+        }
+
+
+        private static void TestAddTableFindMethod()
+        {
+            AddTableFindMethodDialog dialog = new AddTableFindMethodDialog();
+
+            AddTableFindMethodParms parms = new AddTableFindMethodParms();
+            //parms.MethodName = "find3";
+            parms.IsTestMode = true;
+            parms.TableName = "MyTable";
+            parms.Fields = new List<AxTableFieldParm> {
+                new AxTableFieldParm { FieldName = "Field1",    FieldType = "Type1",    IsMandatory = true },
+                new AxTableFieldParm { FieldName = "Field2Big", FieldType = "Type2Big", IsMandatory = true },
+                new AxTableFieldParm { FieldName = "F3",        FieldType = "T3",       IsMandatory = false }
+            };
+
+            dialog.SetParameters(parms);
+
+            Application.Run(dialog);
+        }
+    }
+}
---
+++ InstallToVS/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using Microsoft.Win32;
+
+namespace InstallToVS
+{
+    class Program
+    {
+        private const string DevUtilsDll     = "DEVUtilsD365.dll";
+        private const string DevUtilsPdb     = "DEVUtilsD365.pdb";
+        private const string LabelEditorDLL = "DeveloperToolsLabelsEditor.dll";
+        private const string LabelEditorPdb = "DeveloperToolsLabelsEditor.pdb";
+        private const string AddinFolder = "AddinExtensions";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                //initialize files to copy
+                ArrayList dllArrayList = new ArrayList();
+                dllArrayList.Add(DevUtilsDll);
+                dllArrayList.Add(DevUtilsPdb);
+                dllArrayList.Add(LabelEditorDLL);
+                dllArrayList.Add(LabelEditorPdb);
+                string extensionFolderName = FindExtensionFolder();
+                Console.WriteLine($"VS extension folder: {extensionFolderName}");
+
+                foreach (string fileName in dllArrayList)
+                {
+                    string sourcePath = Path.Combine(Environment.CurrentDirectory, fileName);
+                    string targetPath = Path.Combine(extensionFolderName, fileName);
+                    File.Copy(sourcePath, targetPath, true);
+                }
+
+                Console.WriteLine("Setup finished! Close and enjoy!");
+
+            }
+            catch (Exception ee)
+            {
+                Console.Error.WriteLine(ee);
+                Console.Error.WriteLine("Seems that an issue prevented me from doing my job :(");
+            }
+
+            Console.ReadLine();
+        }
+        private static string FindExtensionFolder()
+        {
+            /*
+            using (var extensionsRegKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\VisualStudio\14.0\ExtensionManager\EnabledExtensions"))
+            {
+                string path = "";
+                if (extensionsRegKey != null)
+                {
+                    string axToolsKeyName = extensionsRegKey.GetValueNames()
+                        .FirstOrDefault(name => name.StartsWith("DynamicsRainierVSTools"));
+                    if (axToolsKeyName != null)
+                    {
+                        path = (string) extensionsRegKey.GetValue(axToolsKeyName);
+                    }
+                }
+                */
+            string path = "";
+            RegistryKey d365Key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\VisualStudio\14.0_Config\AutomationProperties\Dynamics 365");
+            if (d365Key != null)
+            {
+                string package = (string) d365Key.GetValue("Package");
+
+                RegistryKey pathKey =
+                    Registry.CurrentUser.OpenSubKey(
+                        $@"SOFTWARE\Microsoft\VisualStudio\14.0_Config\BindingPaths\{package}");
+                if (pathKey != null)
+                {
+                    path = pathKey.GetValueNames()[0];
+                }
+            }
+
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ApplicationException("Could not find D365FO tools in Windows registry.");
+            }
+            return Path.Combine(path, AddinFolder);
+            //}
+
+        }
+    }
+}
